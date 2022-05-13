c1c2ed03f742237b92967afd4f6f8b15510e2f11
Rudra2109 <82201099+Rudra2109@users.noreply.github.com>
2022-05-13 16:57:17 +0400

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,36 @@
+Status = "";
+tv_image = "";
+
+function preload(){
+    frige_image = loadImage("TV.PNG");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(tv_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw() {
+    image(tv_image, 0, 0, 640, 420);
+
+    fill("#FF0000");
+    text("table", 320 , 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300 ,90 , 270 , 320);
+}
